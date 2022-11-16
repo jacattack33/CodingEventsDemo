3480f09daf6752b539212257a57a509647601713
Jacque <jacquemueller2468@gmail.com>
2022-11-16 16:49:50 -0600

    completed exercises

---
+++ CodingEventsDemo/Data/EventDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CodingEventsDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CodingEventsDemo.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> Categories { get; set; }
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
+        {
+
+        }
+
+           
+    }
+}
